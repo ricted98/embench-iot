cade6d2076f3b0faead1e30e4f80ad1dbecd67c8
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-05-02 10:33:09 +0200

    config: Enable printf usage

--- config/riscv32/boards/cheshire/boardsupport.c
+++ config/riscv32/boards/cheshire/boardsupport.c
@@ -7,22 +7,29 @@
 
    SPDX-License-Identifier: GPL-3.0-or-later */
 
+   #include <stdint.h>
    #include <support.h>
+   #include "regs/cheshire.h"
+   #include "dif/clint.h"
+   #include "dif/uart.h"
+   #include "params.h"
+   #include "util.h"
+   #include "printf.h"
 
    void
    initialise_board ()
    {
-     __asm__ volatile ("li a0, 0" : : : "memory");
+    uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
+    uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
+    uart_init(&__base_uart, reset_freq, __BOOT_BAUDRATE);
    }
 
    void __attribute__ ((noinline)) __attribute__ ((externally_visible))
    start_trigger ()
    {
-     __asm__ volatile ("li a0, 0" : : : "memory");
    }
 
    void __attribute__ ((noinline)) __attribute__ ((externally_visible))
    stop_trigger ()
    {
-     __asm__ volatile ("li a0, 0" : : : "memory");
    }
